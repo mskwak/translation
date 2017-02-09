a367cdade8a52c829b94a88d5cae5a79b77f2e66
DESKTOP-R310QHC\ryu <kdamduck77@gmail.com>
2017-02-09 21:55:16 +0900

    commit!!!

--- src/main/java/kr/co/easymanual/model/EmAttachments.java
+++ src/main/java/kr/co/easymanual/model/EmAttachments.java
@@ -2,6 +2,9 @@ package kr.co.easymanual.model;
 
 import java.util.Date;
 
+import lombok.Data;
+
+@Data
 public class EmAttachments {
     private Integer id;
     private String langset;
@@ -14,102 +17,4 @@ public class EmAttachments {
     private String uploader;
     private Date createdTime;
     private Date updatedTime;
-
-	public Integer getId() {
-		return this.id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getLangset() {
-		return this.langset;
-	}
-
-	public void setLangset(String langset) {
-		this.langset = langset;
-	}
-
-	public String getCountry() {
-		return this.country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getHashName() {
-		return this.hashName;
-	}
-
-	public void setHashName(String hashName) {
-		this.hashName = hashName;
-	}
-
-	public String getPath() {
-		return this.path;
-	}
-
-	public void setPath(String path) {
-		this.path = path;
-	}
-
-	public String getExtension() {
-		return this.extension;
-	}
-
-	public void setExtension(String extension) {
-		this.extension = extension;
-	}
-
-	public Long getSize() {
-		return this.size;
-	}
-
-	public void setSize(Long size) {
-		this.size = size;
-	}
-
-	public String getUploader() {
-		return this.uploader;
-	}
-
-	public void setUploader(String uploader) {
-		this.uploader = uploader;
-	}
-
-	public Date getCreatedTime() {
-		return this.createdTime;
-	}
-
-	public void setCreatedTime(Date createdTime) {
-		this.createdTime = createdTime;
-	}
-
-	public Date getUpdatedTime() {
-		return this.updatedTime;
-	}
-
-	public void setUpdatedTime(Date updatedTime) {
-		this.updatedTime = updatedTime;
-	}
-
-	@Override
-	public String toString() {
-		return "EmAttachments [id=" + this.id + ", langset=" + this.langset
-				+ ", country=" + this.country + ", name=" + this.name + ", hashName="
-				+ this.hashName + ", path=" + this.path + ", extension=" + this.extension
-				+ ", size=" + this.size + ", uploader=" + this.uploader
-				+ ", createdTime=" + this.createdTime + ", updatedTime="
-				+ this.updatedTime + "]";
-	}
 }
--- src/main/java/kr/co/easymanual/model/solr/TbxSolrBean.java
+++ src/main/java/kr/co/easymanual/model/solr/TbxSolrBean.java
@@ -2,6 +2,9 @@ package kr.co.easymanual.model.solr;
 
 import org.apache.solr.client.solrj.beans.Field;
 
+import lombok.Data;
+
+@Data
 public class TbxSolrBean implements SolrBean {
 	@Field
 	private String id;
@@ -20,52 +23,4 @@ public class TbxSolrBean implements SolrBean {
 
 	@Field
 	private String term;
-
-	public String getId() {
-		return this.id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getTermEntryId() {
-		return this.termEntryId;
-	}
-
-	public void setTermEntryId(String termEntryId) {
-		this.termEntryId = termEntryId;
-	}
-
-	public String getLangSet() {
-		return this.langSet;
-	}
-
-	public void setLangSet(String langSet) {
-		this.langSet = langSet;
-	}
-
-	public String getCountry() {
-		return this.country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public String getDescrip() {
-		return this.descrip;
-	}
-
-	public void setDescrip(String descrip) {
-		this.descrip = descrip;
-	}
-
-	public String getTerm() {
-		return this.term;
-	}
-
-	public void setTerm(String term) {
-		this.term = term;
-	}
 }
