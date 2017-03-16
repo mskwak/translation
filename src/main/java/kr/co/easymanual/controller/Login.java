61e1124671c6e08f884706c01102c629b2e34222
DESKTOP-R310QHC\ryu <kdamduck77@gmail.com>
2017-03-16 22:01:12 +0900

    commit!!!

---
+++ src/main/java/kr/co/easymanual/controller/Login.java
@@ -0,0 +1,17 @@
+package kr.co.easymanual.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Login {
+	private static final Logger logger = LoggerFactory.getLogger(SearchTerminology.class);
+
+	@RequestMapping(value = {"/", "/login.do"}, method = {RequestMethod.GET})
+	public String loginGet() {
+		return "login";
+	}
+}
