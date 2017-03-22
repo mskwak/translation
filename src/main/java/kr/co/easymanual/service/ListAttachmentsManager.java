9be8e7bfb068757692875688af8f5c72d9ad0065
DESKTOP-R310QHC\ryu <kdamduck77@gmail.com>
2017-03-22 23:49:54 +0900

    commit!!!

--- src/main/java/kr/co/easymanual/service/ListAttachmentsManager.java
+++ src/main/java/kr/co/easymanual/service/ListAttachmentsManager.java
@@ -1,8 +1,10 @@
-package kr.co.easymanual.service;
+ package kr.co.easymanual.service;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import kr.co.easymanual.entity.EmAttachments;
@@ -20,7 +22,7 @@ public class ListAttachmentsManager {
 	public List<EmAttachments> listAttachments(String fieldName, String sort) {
 		// Hibernate 버전
 		// TODO 가변적인 데이터인 fieldName을 ORDER BY fieldName로 설정하여 spring data에서 지원하는 메서드를 작성할 수 있는가?
-		return this.emAttachmentsRepository.findAll();
+		return this.emAttachmentsRepository.findAll(new Sort(Direction.valueOf(sort), fieldName));
 
 		// MyBatis 버전
 		// <!-- List<EmAttachments> selectAllByField(@Param("fieldName") String fieldName, @Param("sort") String sort); -->
